dac28cc46008c2c3feefb60ff770644a9389da2c
chaudhary-tushar <tushatstriker@gmail.com>
2023-11-18 05:54:11 +0530

    modified api-docs

--- app/app/settings.py
+++ app/app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
     'user',
     'recipe',
@@ -145,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
-REST_FARMEWORK = {
+REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
